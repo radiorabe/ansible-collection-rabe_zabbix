f2a16caaab33442061add6ef6802aa2d53536620
Lucas Bickel <116588+hairmare@users.noreply.github.com>
2024-02-26 00:20:27 +0100

    chore: blacken python code (#17)

--- plugins/modules/zabbix_host_maintenance.py
+++ plugins/modules/zabbix_host_maintenance.py
@@ -8,7 +8,9 @@ from __future__ import absolute_import, division, print_function
 
 from ansible.module_utils.basic import AnsibleModule
 
-from ansible_collections.community.zabbix.plugins.module_utils.base import ZabbixBase  # noqa: E501
+from ansible_collections.community.zabbix.plugins.module_utils.base import (
+    ZabbixBase,
+)  # noqa: E501
 import ansible_collections.community.zabbix.plugins.module_utils.helpers as zabbix_utils  # noqa: E501
 
 
